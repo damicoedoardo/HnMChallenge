9e0c4cf5aacf8af753f749dfaa0b4efe4d050ede
Edoardo <damicoedoardo95@gmail.com>
2022-03-12 15:59:12 +0000

    fixed bug filtered_user

--- hnmchallenge/data_preprocessing/filtered_articles.py
+++ hnmchallenge/data_preprocessing/filtered_articles.py
@@ -1,34 +1,33 @@
-import pandas as pd
-from hnmchallenge.data_reader import DataReader
+import logging
+
 import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
+import pandas as pd
+import seaborn as sns
+from hnmchallenge.constant import *
+from hnmchallenge.data_reader import DataReader
 from hnmchallenge.dataset import Dataset
+from hnmchallenge.evaluation.python_evaluation import map_at_k
 from hnmchallenge.filtered_dataset import FilterdDataset
 from hnmchallenge.models.top_pop import TopPop
-from hnmchallenge.evaluation.python_evaluation import map_at_k
-from hnmchallenge.constant import *
 
-import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-dataset = FilterdDataset()
-dr = DataReader()
+if __name__ == "__main__":
+    dataset = FilterdDataset()
+    dr = DataReader()
 
-full_articles=dr.get_articles()
-filtered=dr.get_filtered_new_raw_mapping_dict()
-cust_dict, article_dict = filtered
+    full_articles = dr.get_articles()
+    filtered = dr.get_filtered_new_raw_mapping_dict()
+    cust_dict, article_dict = filtered
 
-article_frame= pd.DataFrame.from_dict(article_dict,orient='index')
-article_frame=article_frame.reset_index(level=0)
-article_frame=article_frame.rename(columns={0:"article_id"})
+    article_frame = pd.DataFrame.from_dict(article_dict, orient="index")
+    article_frame = article_frame.reset_index(level=0)
+    article_frame = article_frame.rename(columns={0: "article_id"})
 
-article=article_frame.merge(full_articles,on='article_id',how='left')
-article=article.drop("article_id",axis=1)
-article=article.rename(columns={"index":"article_id"})
+    article = article_frame.merge(full_articles, on="article_id", how="left")
+    article = article.drop("article_id", axis=1)
+    article = article.rename(columns={"index": "article_id"})
 
-
-article.to_feather(
-        dr.get_preprocessed_data_path() / "filtered_artilces.feather"
-    )
+    article.to_feather(dr.get_preprocessed_data_path() / "filtered_artilces.feather")
--- hnmchallenge/data_preprocessing/filtered_customers.py
+++ hnmchallenge/data_preprocessing/filtered_customers.py
@@ -1,33 +1,36 @@
-import pandas as pd
-from hnmchallenge.data_reader import DataReader
+import logging
+
 import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
+import pandas as pd
+import seaborn as sns
+from hnmchallenge.constant import *
+from hnmchallenge.data_reader import DataReader
 from hnmchallenge.dataset import Dataset
+from hnmchallenge.evaluation.python_evaluation import map_at_k
 from hnmchallenge.filtered_dataset import FilterdDataset
 from hnmchallenge.models.top_pop import TopPop
-from hnmchallenge.evaluation.python_evaluation import map_at_k
-from hnmchallenge.constant import *
 
-import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-dataset = FilterdDataset()
-dr = DataReader()
+if __name__ == "__main__":
+    dataset = FilterdDataset()
+    dr = DataReader()
 
-full_customers=dr.get_customer()
-full_customers=full_customers.fillna(0)
-filtered=dr.get_filtered_new_raw_mapping_dict()
-cust_dict, article_dict = filtered
+    full_customers = dr.get_customer()
+    full_customers = full_customers.fillna(0)
+    filtered = dr.get_filtered_new_raw_mapping_dict()
+    cust_dict, article_dict = filtered
 
-customer_frame= pd.DataFrame.from_dict(cust_dict,orient='index')
-customer_frame=customer_frame.reset_index(level=0)
-customer_frame=customer_frame.rename(columns={0:"customer_id"})
-customer=customer_frame.merge(full_customers,on='customer_id',how='left')
-customer=customer.drop("customer_id",axis=1)
-customer=customer.rename(columns={"index":"customer_id"})
+    customer_frame = pd.DataFrame.from_dict(cust_dict, orient="index")
+    customer_frame = customer_frame.reset_index(level=0)
+    customer_frame = customer_frame.rename(columns={0: "customer_id"})
+    customer = customer_frame.merge(full_customers, on="customer_id", how="left")
+    customer = customer.drop("customer_id", axis=1)
+    customer = customer.rename(columns={"index": "customer_id"})
 
-customer.to_feather(
-        dr.get_preprocessed_data_path() / "filtered_customers.feather"
-    )
+    customer["club_member_status"] = customer["club_member_status"].astype(str)
+    customer["fashion_news_frequency"] = customer["fashion_news_frequency"].astype(str)
+
+    customer.to_feather(dr.get_preprocessed_data_path() / "filtered_customers.feather")
