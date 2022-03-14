9425d4921d042b5426acf2865a9f4dd2502ad4cb
Asr419 <aayush.singha.roy@iitd.ac.in>
2022-03-14 12:44:37 +0000

    user item feature

--- hnmchallenge/features/user_item_features/sales_channel_id.py
+++ hnmchallenge/features/user_item_features/sales_channel_id.py
@@ -7,25 +7,26 @@ from hnmchallenge.feature_manager import UserItemFeature
 from hnmchallenge.stratified_dataset import StratifiedDataset
 
 
-class ItemPrice(UserItemFeature):
-    FEATURE_NAME = "item_price"
+class SalesChannel(UserItemFeature):
+    FEATURE_NAME = "sales_channel_id"
 
     def __init__(self, dataset: StratifiedDataset, kind: str) -> None:
         super().__init__(dataset, kind)
 
     def _create_feature(self) -> pd.DataFrame:
-        print(self.kind)
         data_df = (
             self.dataset.get_holdin()
             if self.kind == "train"
             else self.dr.get_filtered_full_data()
         )
         feature = (
-            data_df[[DEFAULT_USER_COL, DEFAULT_ITEM_COL, "t_dat"]]
+            data_df[[DEFAULT_USER_COL, DEFAULT_ITEM_COL,"t_dat", "sales_channel_id"]]
             .drop_duplicates()
-            .drop("t_dat", axis=1)
+            
         )
-        feature = feature.rename({"price": self.FEATURE_NAME}, axis=1)
+        feature = data_df[[DEFAULT_USER_COL, DEFAULT_ITEM_COL, "t_dat", "sales_channel_id"]]
+        feature = feature[~(feature[[DEFAULT_USER_COL, DEFAULT_ITEM_COL, "t_dat"]].duplicated())].drop_duplicates()
+        feature = feature.rename({"sales_channel_id": self.FEATURE_NAME}, axis=1)
         return feature
 
 
@@ -33,5 +34,5 @@ class ItemPrice(UserItemFeature):
 if __name__ == "__main__":
     dataset = StratifiedDataset()
     for kind in ["train", "full"]:
-        feature = ItemPrice(dataset, kind)
+        feature = SalesChannel(dataset, kind)
         feature.save_feature()
