1f8acfe1ef3b8bc4e5e7f8b34bedd31d990ab603
Asr419 <aayush.singha.roy@iitd.ac.in>
2022-04-01 15:00:17 +0100

    user average buy feature

---
+++ hnmchallenge/features/user_features/user_average_buy_day.py
@@ -0,0 +1,49 @@
+from unicodedata import name
+
+import numpy as np
+import pandas as pd
+from dotenv import main
+from hnmchallenge.constant import DEFAULT_ITEM_COL, DEFAULT_USER_COL
+from hnmchallenge.dataset import Dataset
+from hnmchallenge.features.feature_interfaces import UserFeature
+from hnmchallenge.stratified_dataset import StratifiedDataset
+
+
+class UserAvgBuyDay(UserFeature):
+    FEATURE_NAME = "user_average_buy_day"
+
+    def __init__(self, dataset, kind: str) -> None:
+        super().__init__(dataset, kind)
+
+    def _create_feature(self) -> pd.DataFrame:
+        data_df = (
+            self.dataset.get_holdin()
+            if self.kind == "train"
+            else self.dr.get_full_data()
+        )
+
+        # filter on the last month_data
+        df = data_df.drop_duplicates(["t_dat", DEFAULT_USER_COL])
+
+        df["date_diff"] = (
+            df.pop("t_dat")
+            .groupby(df["customer_id"])
+            .diff()
+            .dt.days.fillna(0, downcast="infer")
+        )
+
+        user_average = df.groupby(DEFAULT_USER_COL)["date_diff"].mean().reset_index()
+
+        feature = user_average
+        user_df = self._get_keys_df()
+        feature = pd.merge(user_df, feature, on="customer_id", how="left")
+        feature = feature.rename({"date_diff": self.FEATURE_NAME}, axis=1)
+        print(feature)
+        return feature
+
+
+if __name__ == "__main__":
+    dataset = Dataset()
+    for kind in ["full", "train"]:
+        feature = UserAvgBuyDay(dataset, kind)
+        feature.save_feature()
